26855de0a08e7c9dcbfac39083a731f49f73fec1
HansolKim <3031902@gmail.com>
2022-09-04 22:41:58 +0900

    caption add 1

--- run.py
+++ run.py
@@ -5,18 +5,27 @@ from src.runners import S_Runner, LOSO_Runner
 import wandb
 import warnings
 
+#config 파일을 불러오는 경로 설정
 CONFIG_DIR = Path("Configs")
 
-
+#Omegaconf를 이용해서 저장된 config 파일을 불러옴
 def main(cfg=OmegaConf.load(CONFIG_DIR / "config.yaml")) -> None:
+    #Omegaconf를 이용해서 저장된 model에 관한 config 파일을 불러옴
     model_params = OmegaConf.load(CONFIG_DIR / "models.yaml")
+    #두 config 파일을 합침
     cfg = OmegaConf.merge(cfg, model_params)
+    #명령어로 입력한 내용과 config를 합침
     cfg.merge_with_cli()
 
+    #wandb에 저장될 위치와 이름을 정함
     wandb.init(entity="qubit-hanyang", project=f"{cfg.log.project_name}", name=f"{cfg.Simple_QHN.model} {cfg.optimizer.lr},{cfg.Simple_QHN.shift} {cfg.Simple_QHN.backend}")
+    #IBMQ 계정에 로그인, 처음 사용할 경우 save_account 필요
     #IBMQ.save_account("token")
     IBMQ.load_account()
+    #경고 메시지 무시
     warnings.filterwarnings(action='ignore')
+
+    #원하는 종류의 runner를 고르고 config파일에서 변수들을 불러와서 실행
     runner = S_Runner(
         log=cfg.log,
         optimizer=cfg.optimizer,
--- src/models/qhn.py
+++ src/models/qhn.py
@@ -5,29 +5,34 @@ from torch import nn
 import torch.nn.functional as F
 from typing import Optional, Dict
 
-
 from src.layers import Hybrid, Aer_Hybrid
 
-
+#Quantum Hybrid Network 구조
 class Simple_QHN(nn.Module):
     def __init__(self, params: Optional[Dict] = None, *args, **kwargs) -> None:
         super(Simple_QHN, self).__init__()
+        #parameter를 받아와서 각각의 변수를 저장 
         self.params = params.Simple_QHN
         self.no_quantum = self.params.no_quantum
         self.model = self.params.model
         self.simulation = self.params.backend.simulation
         self.lstm_hidden = self.params.lstm_hidden
+        #2개의 convolution layer를 정의
         self.conv1 = nn.Conv1d(116, 6, kernel_size=5)
         self.conv2 = nn.Conv1d(6, 16, kernel_size=5)
+        #dropout 함수 정의
         self.dropout = nn.Dropout2d()
+        #lstm 함수 정의
         self.lstm = nn.LSTM(
             input_size=16,
             hidden_size=self.lstm_hidden,
             num_layers=1,
             bidirectional=True,
         )
+        #양자회로 앞의 dense layer 정의
         self.fc1 = nn.Linear(256, self.params.linear_out)
         self.fc2 = nn.Linear(self.params.linear_out, self.params.n_qubits)
+        #aer 시뮬레이터를 사용하는 경우
         if self.params.backend.backend[0:3] == "aer":
             self.hybrid = Aer_Hybrid(
             model = self.model,
@@ -36,32 +41,43 @@ class Simple_QHN(nn.Module):
             shots = self.params.shots,
             shift = self.params.shift, 
             )
+        #일반적인 backend를 사용하는 경우
         else:
             self.hybrid = Hybrid
+        #양자 회로 뒤의 dense layer 정의
         self.fc3 = nn.Linear(2**self.params.n_qubits, 2)
         self.fc4 = nn.Linear(self.params.n_qubits, 2)
+        #가능한 경우 gpu연산
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def forward(self, x):
+        #1차원 convoultion적용-> relu 함수 적용 -> 1차원 maxpool 2번 반복
         x = F.relu(self.conv1(x))
         x = F.max_pool1d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool1d(x, 2)
+        #데이터의 형태를 바꾸고 lstm을 적용함
         x = torch.flatten(x, 2)
         x, (h, c) = self.lstm(x.permute(0, 2, 1))
         x = torch.concat(
             [x[:, -1, : self.lstm_hidden], x[:, 0, self.lstm_hidden :]], axis=1
         )
+        #데이터를 1차원으로 바꿈 (batch는 유지)
         x = torch.flatten(x, 1)
+        #qubit수와 맞추기 위한 dense layer
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+
         if self.no_quantum:
+            #양자 회로를 사용하지 않는 경우 바로 결과를 내기위한 dense layer를 통과
             x = self.fc4(x)
         else:
+            #입력값을 각도의 형태로 바꾸어 주기 위해서 tanh을 통과시키고 pi/2만큼 곱함
             x = torch.tanh(x) * torch.ones_like(x) * torch.tensor(np.pi / 2)
+            #aer 시뮬레이터를 사용하는 경우, 양자 회로를 통과
             if self.params.backend.backend[0:3] == "aer":
                 x = self.hybrid(x).to(self.device)
-                
+            #일반적인 backend에서 양자회로를 통과
             else: 
                 x = self.hybrid(input = x,
                     model = self.params.model, 
@@ -69,7 +85,9 @@ class Simple_QHN(nn.Module):
                     backend = self.params.backend,
                     shots = self.params.shots,
                     shift = self.params.shift,).forward().to(self.device)
+            #최종 결과를 얻기 위한 dense layer
             x = self.fc3(x)
+        #softmax 함수를 사용해서 확률 형태로 데이터를 만듬
         x = F.softmax(x, dim=1)
         return x
 
